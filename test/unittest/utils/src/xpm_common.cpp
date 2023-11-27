8a9b2c8721fe49f26adcc41e0ab4dc28598de3c1
openharmony_ci <120357966@qq.com>
2023-11-27 07:42:40 +0000

    回退 'Pull Request !35 : code_signature:添加ownerId设置接口'

--- test/unittest/utils/src/xpm_common.cpp
+++ test/unittest/utils/src/xpm_common.cpp
@@ -14,7 +14,6 @@
  */
 
 #include "xpm_common.h"
-
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
@@ -23,28 +22,54 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-
-#include "code_sign_attr_utils.h"
 #include "log.h"
 #include "securec.h"
 
 namespace OHOS {
 namespace Security {
 namespace CodeSign {
+struct XpmRegionInfo {
+    uint64_t base;
+    uint64_t length;
+};
+
 struct XpmRegionArea {
     uint64_t start;
     uint64_t end;
 };
 
+const std::string XPM_DEV_PATH = "/dev/xpm";
 const std::string XPM_PROC_PREFIX_PATH = "/proc/";
 const std::string XPM_PROC_SUFFIX_PATH = "/xpm_region";
 
 constexpr unsigned long XPM_PROC_LENGTH = 50;
+constexpr unsigned long XPM_REGION_LEN = 0x8000000;
+constexpr unsigned long SET_XPM_REGION = _IOW('x', 0x01, struct XpmRegionInfo);
 
-static int GetXpmRegion(struct XpmRegionArea &area)
+static int SetXpmRegion(void)
 {
-    if (InitXpmRegion() != 0) {
-        LOG_ERROR(LABEL, "init xpm region failed");
+    struct XpmRegionInfo info = { 0, XPM_REGION_LEN };
+
+    int fd = open(XPM_DEV_PATH.c_str(), O_RDWR);
+    if (fd < 0) {
+        LOG_ERROR(LABEL, "open xpm dev file failed(%{public}s)", strerror(errno));
+        return -1;
+    }
+
+    int ret = ioctl(fd, SET_XPM_REGION, &info);
+    if (ret < 0) {
+        LOG_ERROR(LABEL, "xpm set region failed(%{public}s)", strerror(errno));
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
+
+static int GetXpmRegion(struct XpmRegionArea *area)
+{
+    if (area == nullptr) {
+        LOG_ERROR(LABEL, "input area is NULL");
         return -1;
     }
 
@@ -63,7 +88,7 @@ static int GetXpmRegion(struct XpmRegionArea &area)
         return -1;
     }
 
-    ret = sscanf_s(xpm_region, "%llx-%llx", &area.start, &area.end);
+    ret = sscanf_s(xpm_region, "%llx-%llx", &area->start, &area->end);
     if (ret < 0) {
         LOG_ERROR(LABEL, "sscanf xpm region string failed(%{public}s)", strerror(errno));
         return -1;
@@ -73,11 +98,33 @@ static int GetXpmRegion(struct XpmRegionArea &area)
     return 0;
 }
 
+static int InitXpmRegion(struct XpmRegionArea *area)
+{
+    if (area == nullptr) {
+        LOG_ERROR(LABEL, "input area is NULL");
+        return -1;
+    }
+
+    int ret = SetXpmRegion();
+    if (ret != 0) {
+        LOG_ERROR(LABEL, "set xpm region failed");
+        return ret;
+    }
+
+    ret = GetXpmRegion(area);
+    if (ret != 0) {
+        LOG_ERROR(LABEL, "get xpm region failed");
+        return ret;
+    }
+
+    return 0;
+}
+
 bool AllocXpmRegion()
 {
     struct XpmRegionArea area = {0};
 
-    if (GetXpmRegion(area)) {
+    if (InitXpmRegion(&area)) {
         return false;
     }
     if (!area.start) {
