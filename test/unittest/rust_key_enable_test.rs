d4c7c55ae8f28d37018d9df9b612d4bc45fac590
fundavid <fangjiawei8@huawei.com>
2024-12-09 10:36:13 +0800

    adhoc sync
    Signed-off-by: fundavid <fangjiawei8@huawei.com>

--- test/unittest/rust_key_enable_test.rs
+++ test/unittest/rust_key_enable_test.rs
@@ -13,8 +13,14 @@
  * limitations under the License.
  */
 extern crate key_enable;
+extern crate ylong_json;
+
+use std::thread;
+use ylong_json::JsonValue;
 use key_enable::cert_chain_utils::PemCollection;
 use key_enable::cert_path_utils::TrustCertPath;
+use key_enable::profile_utils::{UDID, get_udid, validate_bundle_and_distribution_type};
+
 
 // pem_cert_file
 const VALID_PEM_CERT: &str = "/data/test/tmp/valid_pem_cert.json";
@@ -98,4 +104,357 @@ fn test_empty_cert_path_json_file() {
         cert_paths.app_sources.is_empty(),
         "Expected cert_paths.app_sources to be empty for an empty JSON file"
     );
+}
+
+#[test]
+fn test_parse_enterprise_profile() {
+    let profile_str = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "enterprise",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.enterprise",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        }
+    }
+    "#;
+    let profile_json =JsonValue::from_text(profile_str).unwrap();
+    let result = validate_bundle_and_distribution_type(&profile_json, true);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_parse_enterprise_normal_profile() {
+    let profile_str = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "enterprise_normal",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.enterprise_normal",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        }
+    }
+    "#;
+    let profile_json =JsonValue::from_text(profile_str).unwrap();
+    let result = validate_bundle_and_distribution_type(&profile_json, true);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_parse_enterprise_mdm_profile() {
+    let profile_str = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "enterprise_mdm",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.enterprise_mdm",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        }
+    }
+    "#;
+    let profile_json =JsonValue::from_text(profile_str).unwrap();
+    let result = validate_bundle_and_distribution_type(&profile_json, true);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_parse_debug_profile() {
+    let profile_str = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "developer",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "debug",
+        "bundle-info": {
+            "developer-id": "",
+            "development-certificate": "",
+            "bundle-name": "com.test.developer",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        },
+        "debug-info": {
+            "device-ids": [],
+            "device-id-type": "udid"
+        }
+    }
+    "#;
+    let udid = get_udid().expect("Failed to get UDID");
+    let mut profile_json =JsonValue::from_text(profile_str).unwrap();
+    profile_json["debug-info"]["device-ids"][0] = JsonValue::String(udid);
+    let result = validate_bundle_and_distribution_type(&profile_json, true);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_parse_iternaltesting_profile() {
+    let profile_str = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "internaltesting",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.internaltesting",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        },
+        "debug-info": {
+            "device-ids": [],
+            "device-id-type": "udid"
+        }
+    }
+    "#;
+    let udid = get_udid().expect("Failed to get UDID");
+    let mut profile_json =JsonValue::from_text(profile_str).unwrap();
+    profile_json["debug-info"]["device-ids"][0] = JsonValue::String(udid);
+    let result = validate_bundle_and_distribution_type(&profile_json, true);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_parse_invalid_profile() {
+    let no_type_profile = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "internaltesting",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.internaltesting",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        },
+        "debug-info": {
+            "device-ids": [],
+            "device-id-type": "udid"
+        }
+    }
+    "#;
+    let no_distribution_profile = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.internaltesting",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        },
+        "debug-info": {
+            "device-ids": [],
+            "device-id-type": "udid"
+        }
+    }
+    "#;
+    let no_debug_info_profile = r#"
+    {
+        "version-name": "2.0.0",
+        "version-code": 2,
+        "app-distribution-type": "internaltesting",
+        "uuid": "",
+        "validity": {
+            "not-before": 1,
+            "not-after": 2
+        },
+        "type": "release",
+        "bundle-info": {
+            "developer-id": "",
+            "distribution-certificate": "",
+            "bundle-name": "com.test.internaltesting",
+            "apl": "normal",
+            "app-feature": "test_app",
+            "app-identifier": "123123"
+        },
+        "acls": {
+            "allowed-acls": [
+                ""
+            ]
+        },
+        "app-privilege-capabilities": [],
+        "permissions": {
+            "restricted-permissions": [
+                ""
+            ]
+        }
+    }
+    "#;
+    let udid = get_udid().expect("Failed to get UDID");
+    let mut no_type_profile_json =JsonValue::from_text(no_type_profile).unwrap();
+    no_type_profile_json["debug-info"]["device-ids"][0] = JsonValue::String(udid.clone());
+    let result = validate_bundle_and_distribution_type(&no_type_profile_json, true);
+    assert!(result.is_err());
+
+    let mut no_distribution_profile_json =JsonValue::from_text(no_distribution_profile).unwrap();
+    no_distribution_profile_json["debug-info"]["device-ids"][0] = JsonValue::String(udid.clone());
+    let result = validate_bundle_and_distribution_type(&no_distribution_profile_json, true);
+    assert!(result.is_err());
+
+    let no_debug_info_profile_json =JsonValue::from_text(no_debug_info_profile).unwrap();
+    let result = validate_bundle_and_distribution_type(&no_debug_info_profile_json, true);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_get_udid_once() {
+    let udid_from_get = get_udid().expect("Failed to get UDID");
+    let udid_from_global = UDID.clone().expect("UDID is None");
+
+    assert_eq!(udid_from_get, udid_from_global);
+}
+
+#[test]
+fn test_get_udid_concurrent() {
+    let num_threads = 10;
+    let mut handles = vec![];
+
+    for _ in 0..num_threads {
+        let handle = thread::spawn(|| {
+            let udid = get_udid().expect("Failed to get UDID");
+            assert_eq!(udid, UDID.clone().expect("UDID is None"));
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().expect("Thread panicked");
+    }
 }
